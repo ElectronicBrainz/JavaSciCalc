bcc0c14a9b1cce307ef8f548b584527bac809e18
sitarameez <sitarablossom@gmail.com>
2021-06-26 15:34:46 -0400

    updated log,trig and factorial

--- src/main/java/com/zipcodewilmington/scientificcalculator/ScientificCalculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificCalculator.java
@@ -1,27 +1,111 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class ScientificCalculator {
-    public String switchDisplayMode(String options){
+    public String switchDisplayMode(String options) {
 
         return options;
     }
-    public String DisplayMode(String mode){
+
+    public String DisplayMode(String mode, Integer input) {
         String displayMode = "";
-        Integer i = 0 ;
-        switch(mode){
+        switch (mode) {
             case "binary":
-                displayMode = i.toBinaryString(0);
+                displayMode = Integer.toBinaryString(input);
                 break;
             case "octal":
-                displayMode = i.toOctalString(0);
+                displayMode = Integer.toOctalString(input);
                 break;
             case "decimal":
-                displayMode = String.valueOf(i);
+                displayMode = String.valueOf(input);
                 break;
             case "hexadecimal":
-                displayMode = i.toHexString(0);
+                displayMode = Integer.toHexString(input);
                 break;
         }
         return displayMode;
     }
+
+    public String switchUnitsMode(String options) {
+
+        return options;
+    }
+
+    public String switchUnitsMode(String mode, double input) {
+        String displayMode = "";
+        switch (mode) {
+            case "degrees":
+                displayMode = String.valueOf(Math.toDegrees(input));
+                break;
+            case "radians":
+                displayMode = String.valueOf(Math.toRadians(input));
+                break;
+        }
+        return displayMode;
+    }
+
+    public String trigonometricFunctions(String mode, double input, String inputMode) {
+        double trignometricResult = 0;
+        String result = "";
+        switch (mode) {
+            case "sine":
+                trignometricResult = Math.toDegrees(Math.sin(input));
+                break;
+            case "tangent":
+                trignometricResult = Math.tan(input);
+                break;
+            case "cosine":
+                trignometricResult = Math.cos(input);
+                break;
+            case "inversesine":
+                trignometricResult = Math.asin(input);
+                break;
+            case "inversetan":
+                trignometricResult = Math.atan(input);
+                break;
+            case "inversecosine":
+                trignometricResult = Math.acos(input);
+                break;
+        }
+        result = String.valueOf(trignometricResult);
+        if (inputMode == "degrees") {
+            result = switchUnitsMode(inputMode, input);
+        }
+
+        return result;
+    }
+
+    public String logarithmicFunctions(String mode, double input) {
+        double result = 0;
+        switch (mode) {
+            case "natural log":
+                result = Math.log(input);
+                break;
+            case "inverse log":
+                result = Math.log10(input);
+                break;
+            //case "log":
+            // result = Math.log
+            //break;
+            //case "inverse natural log":
+            // result =;
+            //break;
+        }
+        return String.valueOf(result);
+    }
+
+    public Integer factorialFunction(Integer input) {
+        Integer factorial=0;
+
+        if (input == 0) {
+            factorial = 1;
+        }
+        else if(input>0)
+         {
+            factorial = input * factorialFunction(input - 1);
+        }
+        return factorial;
+    }
+
 }
+
+
--- src/test/java/com/zipcodewilmington/scientificcalculator/ScientificCalculatorTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/ScientificCalculatorTest.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScientificCalculatorTest {
@@ -14,9 +17,88 @@ class ScientificCalculatorTest {
 
     @org.junit.jupiter.api.Test
     void switchDisplayMode() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        String result = scientificCalculator.switchDisplayMode("binary");
+        Assert.assertEquals("binary",result);
     }
 
     @org.junit.jupiter.api.Test
     void displayMode() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        String result = scientificCalculator.DisplayMode("decimal",7);
+        Assert.assertEquals("101",result);
+    }
+
+    @Test
+    void switchUnitsMode() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        String result =scientificCalculator.switchUnitsMode("degrees",8);
+        Assert.assertEquals("0.13962634015954636",result);
+    }
+
+    @Test
+    void testSwitchUnitsMode() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        String result =scientificCalculator.switchUnitsMode("degrees");
+        Assert.assertEquals("degrees",result);
+
+    }
+
+    @Test
+    void trigonometricFunctions() {
+        //given
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+//        //when
+//        String resultForRadians = scientificCalculator.trigonometricFunctions("sine",10,"radians");
+//        //then
+//        Assert.assertEquals("-0.5440211108893698",resultForRadians);
+        //when
+        String resultForDegrees = scientificCalculator.trigonometricFunctions("sine",10,"degrees");
+        //then
+        Assert.assertEquals("-0.5440211108893698",resultForDegrees);
+    }
+
+    @Test
+    void logarithmicFunctions() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+    //given
+        String modeNaturalLog = "natural log";
+        Integer inputForNaturalLog = 10;
+
+    //when
+        String resultForLogarithm = scientificCalculator.logarithmicFunctions(modeNaturalLog,inputForNaturalLog);
+    //then
+        Assert.assertEquals("2.302585092994046",resultForLogarithm);
+    //given
+        String modeInverselog = "inverse log";
+        Integer inputForInverseLog = 10;
+    //when
+        String resultForLogarithm1 = scientificCalculator.logarithmicFunctions(modeInverselog,inputForInverseLog);
+    //Then
+        Assert.assertEquals("10000000000",resultForLogarithm1);
+    }
+
+    @Test
+    void factorialFunction() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        Integer resultOfFactorial;
+    //given
+        Integer inputForPositiveValue = 4;
+    //when
+        resultOfFactorial = scientificCalculator.factorialFunction(inputForPositiveValue);
+    //then
+        Assert.assertEquals(Integer.valueOf(24),resultOfFactorial);
+    //given
+        Integer inputForNegativeValue = -1;
+    //when
+        resultOfFactorial = scientificCalculator.factorialFunction(inputForNegativeValue);
+    //then
+        Assert.assertEquals(Integer.valueOf(0),resultOfFactorial);
+    //given
+        Integer inputForZero = 0;
+    //when
+        resultOfFactorial = scientificCalculator.factorialFunction(inputForZero);
+    //then
+        Assert.assertEquals(Integer.valueOf(1),resultOfFactorial);
     }
 }
