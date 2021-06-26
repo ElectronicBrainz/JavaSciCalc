5f8daba791283341468352ff47507746f0803e25
sitarameez <sitarablossom@gmail.com>
2021-06-26 17:45:05 -0400

    updated unit test to individual methods

--- src/test/java/com/zipcodewilmington/scientificcalculator/ScientificCalculatorTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/ScientificCalculatorTest.java
@@ -23,31 +23,49 @@ class ScientificCalculatorTest {
     }
 
     @org.junit.jupiter.api.Test
-    void displayMode() {
+    void displayModeForBinary() {
         ScientificCalculator scientificCalculator = new ScientificCalculator();
         Integer input = 0;
         String result = "";
-    //given
+        //given
         input = 5;
         String modeForBinary = "binary";
-    //when
-        result = scientificCalculator.DisplayMode(modeForBinary,input);
-    //then
-        Assert.assertEquals("101",result);
-    //given
+        //when
+        result = scientificCalculator.DisplayMode(modeForBinary, input);
+        //then
+        Assert.assertEquals("101", result);
+    }
+    @org.junit.jupiter.api.Test
+    void displayModeForDecimal() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        Integer input = 0;
+        String result = "";
+        //given
         input = 5;
         String modeForDecimal = "decimal";
-    //when
-        result = scientificCalculator.DisplayMode(modeForDecimal,input);
-    //then
-        Assert.assertEquals("5",result);
-    //given
+        //when
+        result = scientificCalculator.DisplayMode(modeForDecimal, input);
+        //then
+        Assert.assertEquals("5", result);
+    }
+    @org.junit.jupiter.api.Test
+    void displayModeForHexaDecimal() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        Integer input = 0;
+        String result = "";
+        //given
         input = 88;
         String modeForHexaDecimal = "hexadecimal";
-    //when
-        result = scientificCalculator.DisplayMode(modeForHexaDecimal,input);
-    //then
-        Assert.assertEquals("58",result);
+        //when
+        result = scientificCalculator.DisplayMode(modeForHexaDecimal, input);
+        //then
+        Assert.assertEquals("58", result);
+    }
+    @org.junit.jupiter.api.Test
+    void displayModeForOctal() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        Integer input = 0;
+        String result = "";
     //given
         input = 88;
         String modeForOctal = "octal";
@@ -87,7 +105,7 @@ class ScientificCalculatorTest {
     }
 
     @Test
-    void logarithmicFunctions() {
+    void logarithmicFunctionsForNaturalLog() {
         ScientificCalculator scientificCalculator = new ScientificCalculator();
     //given
         String modeNaturalLog = "natural log";
@@ -97,36 +115,52 @@ class ScientificCalculatorTest {
         String resultForLogarithm = scientificCalculator.logarithmicFunctions(modeNaturalLog,inputForNaturalLog);
     //then
         Assert.assertEquals("2.302585092994046",resultForLogarithm);
-    //given
-        String modeInverselog = "inverse log";
+    }
+
+    @Test
+    void logarithmicFunctionsForInverseLog() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        //given
+        String modeInverseLog = "inverse log";
         Integer inputForInverseLog = 10;
-    //when
-        String resultForLogarithm1 = scientificCalculator.logarithmicFunctions(modeInverselog,inputForInverseLog);
-    //Then
+        //when
+        String resultForLogarithm1 = scientificCalculator.logarithmicFunctions(modeInverseLog,inputForInverseLog);
+        //Then
         Assert.assertEquals("10000000000",resultForLogarithm1);
     }
 
     @Test
-    void factorialFunction() {
+    void factorialFunctionForPositive() {
         ScientificCalculator scientificCalculator = new ScientificCalculator();
         Integer resultOfFactorial;
-    //given
+        //given
         Integer inputForPositiveValue = 4;
-    //when
+        //when
         resultOfFactorial = scientificCalculator.factorialFunction(inputForPositiveValue);
-    //then
-        Assert.assertEquals(Integer.valueOf(24),resultOfFactorial);
-    //given
+        //then
+        Assert.assertEquals(Integer.valueOf(24), resultOfFactorial);
+    }
+    @Test
+    void factorialFunctionForNegative() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        Integer resultOfFactorial;
+        //given
         Integer inputForNegativeValue = -1;
-    //when
+        //when
         resultOfFactorial = scientificCalculator.factorialFunction(inputForNegativeValue);
-    //then
-        Assert.assertEquals(Integer.valueOf(0),resultOfFactorial);
+        //then
+        Assert.assertEquals(Integer.valueOf(0), resultOfFactorial);
+    }
+    @Test
+    void factorialFunctionForZero() {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        Integer resultOfFactorial;
     //given
         Integer inputForZero = 0;
     //when
         resultOfFactorial = scientificCalculator.factorialFunction(inputForZero);
     //then
         Assert.assertEquals(Integer.valueOf(1),resultOfFactorial);
+        }
     }
 }
